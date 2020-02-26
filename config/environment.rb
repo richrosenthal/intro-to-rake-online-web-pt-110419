aa3daf05a41c3f3808723e15f1d70d3f564c9183
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2020-02-26 03:31:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,3 @@ require_relative "../lib/student.rb"
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 
-namespace :db do
-  desc 'migrate changes to your database'
-  task :migrate => :environment do
-    Student.create_table
-  end
-end
